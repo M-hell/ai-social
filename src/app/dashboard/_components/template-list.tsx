74ad705873671d47d558180ec7078f59dbf56e2d
M-hell <sarkarsamrat16112004@gmail.com>
2025-03-09 23:13:31 +0530

    g

--- src/app/dashboard/_components/template-list.tsx
+++ src/app/dashboard/_components/template-list.tsx
@@ -3,17 +3,13 @@
 import { useEffect, useState } from "react";
 import { contentTemplates } from "@/lib/content-templates";
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
 export const TemplateList = ({ searchInput }: { searchInput: string }) => {
-  const [templateList, setTemplateList] = useState(contentTemplates);
-  const [searchCategory, setSearchCategory] = useState<string | null>(null);
+  const searchParams = useSearchParams();
+  const searchCategory = searchParams.get("category"); // Directly use it
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const searchParams = new URLSearchParams(window.location.search);
-      setSearchCategory(searchParams.get("category"));
-    }
-  }, []);
+  const [templateList, setTemplateList] = useState(contentTemplates);
 
   useEffect(() => {
     if (searchCategory === "All") {
@@ -25,6 +21,7 @@ export const TemplateList = ({ searchInput }: { searchInput: string }) => {
     }
   }, [searchCategory]);
 
+  // Search Input
   useEffect(() => {
     if (searchInput && searchInput.length > 2) {
       setTemplateList(contentTemplates.filter((item) =>
@@ -39,7 +36,10 @@ export const TemplateList = ({ searchInput }: { searchInput: string }) => {
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-5 mt-5">
       {templateList.map((template) => (
         <div key={template.slug}>
-          <Link href={`/dashboard/${template.slug}`} className="bg-white w-full rounded-lg h-[200px] py-4 px-4 text-center flex flex-col justify-center">
+          <Link
+            href={`/dashboard/${template.slug}`}
+            className="bg-white w-full rounded-lg h-[200px] py-4 px-4 text-center flex flex-col justify-center"
+          >
             <template.icon className="h-12 w-12 mx-auto"></template.icon>
             <h2 className="font-semibold mt-5">{template.name}</h2>
           </Link>
